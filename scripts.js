2b93df324c29ee1edfca8a35f112f3da80401b0b
Anna Volkova <annie_v@bk.ru>
2017-10-15 23:44:51 +0300

    modify scripts.js

--- scripts.js
+++ scripts.js
@@ -1,100 +1,125 @@
-let tasks = [
-  // {
-    // 'id': 1,
-    // 'value': 'помыть машину',
-    // 'done': true
-  // }
-]
+// module.exports = (function() {
 
-const taskTemplate = '<li data-id={{id}} class="task {{checked}}"><input class="js-task-checkbox task__checkbox" checked type="checkbox"/>{{value}}<div class="js-delete task__delete">x</div></li>'
+// document.addEventListener('DOMContentLoaded', start)
+//
+// function start() {
+//   const todo = new Todo()
+//   todo.init(document.body)
+// }
 
-document.addEventListener('DOMContentLoaded', start);
 
-function start() {
-	drawForm()
-}
+class Todo {
+  tasks = []
 
-function drawForm() {
-	const app = document.getElementById('app');
-	const html = '<form id="js-newtask-form" class="new-task"><input name="newTask" class="js-input new-task__input" type="text" value="" placeholder="Add new task"/><button class="new-task__button">Add</button></form>'
-	const form = document.createElement('form')
-  app.appendChild(form)
-	form.outerHTML = html
-  const appendedForm = document.querySelector('#js-newtask-form')
-	appendedForm.addEventListener('submit', addNewTask)
-}
+  init = (el) => {
+    let app = document.createElement('div')
+    app.id = 'todo-app'
 
-function addNewTask(event) {
-	event.preventDefault()
-	const form = event.target
-	const input = form.elements.newTask
-  const value = input.value
+    app = el.appendChild(app)
 
-  if (value.length > 0) {
-    tasks.push({'id':tasks.length+1, 'value': value, 'done': false })
-  }
-
-  drawTasks()
-  document.querySelector('.js-input').value = ''
-}
-
-function drawTasks(task) {
-  const ul = document.createElement('ul')
-  ul.className = 'tasks'
-  const app = document.getElementById('app');
-	const pattern = /\schecked/
-  const li = tasks.map(function(el) {
-    const temp = el.done ? taskTemplate : taskTemplate.replace(pattern, '')
-    const checked = el.done ? 'is-checked' : ''
-    const task = Object.assign({checked: checked}, el)
-    return templater(temp)(task)
-  }).join('')
-	const findUl = document.querySelector('ul.tasks');
-	ul.innerHTML = li
-	if (findUl) {
-		app.replaceChild(ul, findUl)
+    this.drawForm(app)
 	}
-	else {
-  	app.appendChild(ul)
+
+	drawForm = (app) => {
+		const html = '<form id="js-newtask-form" class="new-task"><input name="newTask" class="js-input new-task__input" type="text" value="" placeholder="Add new task"/><button class="new-task__button">Add</button></form>'
+		const form = document.createElement('form')
+		app.appendChild(form)
+		form.outerHTML = html
+		const appendedForm = document.querySelector('#js-newtask-form')
+		appendedForm.addEventListener('submit', this.addNewTask)
 	}
-	const del = [].slice.call(document.querySelectorAll('.js-delete'))
-	del.forEach(function(el) {
-		return el.addEventListener('click', deleteTask)
-	})
 
-	const checkbox = [].slice.call(document.querySelectorAll('.js-task-checkbox'))
-	checkbox.forEach(function(el) {
-		return el.addEventListener('change', changeTaskState)
-	})
-}
+	addNewTask = (event) => {
+		event.preventDefault()
 
-function templater(html) {
-  return function(data) {
-      for (var x in data) {
-        var re = '{{\\s?' + x + '\\s?}}'
-        html = html.replace(new RegExp(re, 'ig'), data[x])
-      }
-    return html
-  }
-}
+		const form = event.target
+		const input = form.elements.newTask
+		const value = input.value
 
-function changeTaskState(event) {
-  const checked = event.target.checked
-	const task = event.target.parentNode
-	const taskID = task.dataset.id
-	tasks = tasks.map(function(el) {
-		if (el.id == taskID) {
-			el.done = checked
+		if (value.length > 0) {
+			tasks.push({'id':tasks.length+1, 'value': value, 'done': false })
 		}
-		return el
-	})
-}
 
-function deleteTask(event) {
-	const task = event.target.parentNode
-	const taskID = task.dataset.id
-	tasks = tasks.filter(function(el) {
-		return el.id != taskID
-	})
-	drawTasks()
+		this.drawTasks()
+		document.querySelector('.js-input').value = ''
+	}
+
+	drawTasks = () => {
+		window.taskTemplate = '<li data-id={{id}} class="task {{checked}}"><input class="js-task-checkbox task__checkbox" checked type="checkbox"/>{{value}}<div class="js-delete task__delete">x</div></li>'
+
+		const ul = document.createElement('ul')
+		ul.className = 'tasks'
+		const app = document.getElementById('todo-app');
+		const pattern = /\schecked/
+		const li = tasks.map((el) => {
+			const temp = el.done ? taskTemplate : taskTemplate.replace(pattern, '')
+			const checked = el.done ? 'is-checked' : ''
+			const task = Object.assign({checked: checked}, el)
+			return this.templater(temp)(task)
+		}).join('')
+		const findUl = document.querySelector('ul.tasks');
+		ul.innerHTML = li
+		if (findUl) {
+			app.replaceChild(ul, findUl)
+		}
+		else {
+			app.appendChild(ul)
+		}
+		const del = [].slice.call(document.querySelectorAll('.js-delete'))
+		del.forEach(function(el) {
+			return el.addEventListener('click', this.deleteTask)
+		})
+
+		const checkbox = [].slice.call(document.querySelectorAll('.js-task-checkbox'))
+		checkbox.forEach((el) => {
+			return el.addEventListener('change', this.changeTaskState)
+		})
+	}
+
+	templater = (html) => {
+		return function(data) {
+			for (var x in data) {
+				var re = '{{\\s?' + x + '\\s?}}'
+				html = html.replace(new RegExp(re, 'ig'), data[x])
+			}
+			return html
+		}
+	}
+
+	changeTaskState = (event) => {
+		const checked = event.target.checked
+		const task = event.target.parentNode
+		const taskID = task.dataset.id
+		tasks = tasks.map(function(el) {
+			if (el.id == taskID) {
+				el.done = checked
+			}
+			return el
+		})
+	}
+
+	deleteTask = (event) => {
+		const task = event.target.parentNode
+		const taskID = task.dataset.id
+		tasks = tasks.filter(function(el) {
+			return el.id != taskID
+		})
+		this.drawTasks()
+	}
 }
+// })
+
+
+
+
+
+
+
+
+
+//
+// document.addEventListener('DOMContentLoaded', start);
+//
+// function start() {
+// 	drawApp(document.body)
+// }
