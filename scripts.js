23dc8091e936822c1426ade92d739d24d1c891ee
Anna Volkova <annie_v@bk.ru>
2017-10-15 20:48:43 +0300

    modify scripts

--- scripts.js
+++ scripts.js
@@ -1,17 +1,17 @@
 const tasks = [
-  {
-    'id': 1,
-    'value': 'помыть машину',
-    'done': true
-  },
-  {
-    'id': 2,
-    'value': 'купить продукты',
-    'done': false
-  }
+  // {
+    // 'id': 1,
+    // 'value': 'помыть машину',
+    // 'done': true
+  // },
+  // {
+    // 'id': 2,
+    // 'value': 'купить продукты',
+    // 'done': false
+  // }
 ]
 
-const taskTemplate = '<li data-id={{id}} class="task {{checked}}"><input class="js-task-checkbox task__checkbox" checked="{{checked}}" type="checkbox"/>{{value}}<div class="task__delete">x</div></li>'
+const taskTemplate = '<li data-id={{id}} class="task {{checked}}"><input class="js-task-checkbox task__checkbox" checked="{{checked}}" type="checkbox"/>{{value}}<div class="js-delete task__delete">x</div></li>'
 
 document.addEventListener('DOMContentLoaded', start);
 
@@ -23,7 +23,7 @@ function start() {
 
 function drawForm() {
 	const app = document.getElementById('app');
-	const html = '<form id="js-newtask-form" class="new-task"><input name="newTask" class="new-task__input" type="text" value="" placeholder="Add new task"/><button class="new-task__button">Add</button></form>'
+	const html = '<form id="js-newtask-form" class="new-task"><input name="newTask" class="js-input new-task__input" type="text" value="" placeholder="Add new task"/><button class="new-task__button">Add</button></form>'
 	const form = document.createElement('form')
   app.appendChild(form)
 	form.outerHTML = html
@@ -40,9 +40,10 @@ function addNewTask(event) {
   if (value.length > 0) {
     tasks.push({'id':tasks.length+1, 'value': value, 'done': false })
   }
-  console.log(tasks);
 
   drawTasks()
+  document.querySelector('.js-input').value = ''
+  // console.log(typeof value);
 }
 
 function drawTasks(task) {
@@ -55,8 +56,15 @@ function drawTasks(task) {
     const task = Object.assign({checked: checked}, el)
     return templater(temp)(task)
   }).join('')
-  ul.innerHTML = li
-  app.appendChild(ul)
+  // if (ul) {
+    ul.innerHTML = li
+    // console.log('hello');
+    // ul.insertBefore(li, ul.childNodes[0])
+  // }
+  // else {
+    app.appendChild(ul)
+    // console.log('world');
+  // }
 }
 
 function templater(html) {
@@ -70,11 +78,32 @@ function templater(html) {
 }
 
 function changeTaskState(id) {
-  // const task = document.querySelectorAll('.task')
+  const task = document.querySelectorAll('.task')
 
-  // input.addEventListener('change', changeTaskState)
+  console.log(task);
+  task.addEventListener('change', changeTaskState)
 }
 
 function deleteTask(id) {
+  if (tasks.length > 1) {
+    for (var i=0; i<tasks.length; i++) {
+      var del = document.querySelectorAll('.js-delete')
+      del.addEventListener('click', changeTaskState)
+      // delete this.tasks[i]
+    }
+    // console.log('hello');
 
+  // del.addEventListener( 'click' , function(el) {
+  //   console.log('hello');
+
+  }
+    // li.remove();
+    // saveState();
+    // return false;
+  // });
+
+}
+
+function changeTaskState() {
+  console.log('hello');
 }
